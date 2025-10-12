444200d5f68865c0ae6b81d74b4fc964c3ce7061
mazzzuu <marco.mazzucato.univr@gmail.com>
2025-10-12 13:59:34 +0200

    deleted things

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import Contatti from "./Contatti";
 import Navbar from "./Navbar";
-// import Login from "./Login";
-// import Eventi from "./Eventi";
 import Layout from "./Layot";
-// import EditPage from "./EditPage";
+
 
 function App() {
   return (
@@ -15,9 +13,6 @@ function App() {
         <Routes>
           <Route path="/home" element={<Home />} />
           <Route path="/contatti" element={<Contatti />} />
-          {/* <Route path="/admin" element={<Login />} /> */}
-          {/* <Route path="/eventi" element={<Eventi />} /> */}
-          {/* <Route path="/edit-event/:id" element={<EditPage />} />  */}
           
         </Routes>
       </Layout>
