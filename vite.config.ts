d56af50cadf93e441bdf78aabaf82fadeba43ff2
mazzzuu <marco.mazzucato.univr@gmail.com>
2025-10-23 10:16:48 +0200

    fix: remove sitemap plugin and add static sitemap

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,8 @@
+// vite.config.ts or vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from 'tailwindcss';
-
-// Import dinamico per evitare errori TypeScript
-const sitemap = require('vite-plugin-sitemap').default;
-
-const routes = [
-  { path: '/', lastmod: '2025-10-23' },
-  { path: '/home', lastmod: '2025-10-23' },
-  { path: '/about', lastmod: '2025-10-23' },
-  { path: '/contatti', lastmod: '2025-10-23' },
-];
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    sitemap({
-      baseUrl: 'https://nuovanaturopatia.it',
-      routes,
-    }),
-  ],
+  plugins: [react(),tailwindcss(),],
 });
