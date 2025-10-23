1faa054f29b23429e4f95da26e5220e40820e587
mazzzuu <marco.mazzucato.univr@gmail.com>
2025-10-23 09:57:46 +0200

    sitemap

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,17 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(),tailwindcss(), VitePluginSitemap({
+      baseUrl: 'https://nuovanatuopatia.it', // SOSTITUISCI con il tuo dominio
+      outDir: 'dist',
+      contentBase: 'dist',
+      routes: [
+        { path: '/', lastmod: '2025-10-23' },
+        { path: '/home', lastmod: '2025-10-23' },
+        { path: '/contatti', lastmod: '2025-10-23' },
+        // Aggiungi tutte le tue route qui
+      ],
+    }),
+  ],
 
 });
