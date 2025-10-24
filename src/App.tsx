90cff0539ef82ef2c9b31d205b4624031c697388
mazzzuu <marco.mazzucato.univr@gmail.com>
2025-10-24 12:17:34 +0200

    changed browser title

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import Home from "./Home";
 import Contatti from "./Contatti";
 import Navbar from "./Navbar";
 import Layout from "./Layot";
 
+// Componente per gestire il titolo
+function TitleHandler() {
+  const location = useLocation();
+
+  useEffect(() => {
+    document.title = "Nuova Naturopatia";
+    window.scrollTo(0, 0);
+  }, [location]);
+
+  return null;
+}
+
 function App() {
   return (
     <Router>
+      <TitleHandler />
       <Navbar />
       <Layout>
         <Routes>
