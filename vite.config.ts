6f1dfd9c97edf0b5029466a073f8cecae01ea11c
mazzzuu <marco.mazzucato.univr@gmail.com>
2026-09-22 11:31:42 +0200

    Spostato progetto nella root del repository

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
 // vite.config.ts or vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-
+  plugins: [react(),tailwindcss(),],
 });
